3b4e298a6aea83820766d7ed50f053790f17bb6b
Monica <monica.rolle13@gmail.com>
2021-02-22 12:22:25 +0100

    changes

--- Ejercicio1/app.js
+++ Ejercicio1/app.js
@@ -1,36 +1,25 @@
 // 
 
 
-function calcularPesoIdeal(sexo,altura){
-    var factor = sexo ==="hombre"? 0.75: 0.6;
-    return (altura * 100 - 150) * factor + 50;  
-}
+var calcularPesoIdeal = (sexo, altura) => (altura * 100 - 150) * (sexo === "hombre" ? 0.75 : 0.6) + 50;
 
-function calcularExcesoDePeso(peso ,pesoIdeal){
-    return peso-pesoIdeal;
-}
+var calcularExcesoDePeso = (peso ,pesoIdeal) => peso-pesoIdeal;
 
-function notificaPesoIdeal(pesoIdeal){
-    return "tu peso ideal corresponde a " + pesoIdeal +  " kilos";
-}
+var notificaPesoIdeal = (pesoIdeal) => "tu peso ideal corresponde a " + pesoIdeal +  " kilos";
 
 function notificaEstadoSobrepeso(excesoPeso){
     if(excesoPeso >= 1){
-        return "Cuidado exceso de peso. debes adelgazar ", excesoPeso 
+        return "Cuidado exceso de peso. debes adelgazar "+ excesoPeso + " kilos" ;
     } else if(excesoPeso <= -1){
         return "come un poco mas, debes recuperar " + excesoPeso * -1
     }else{
         return "estas ideal de la muerte";
     }
 }
-
-function informePesoIdeal(altura, peso, sexo){
+var informePesoIdeal = (altura, peso, sexo)  => {
     var pesoIdeal= calcularPesoIdeal(sexo,altura);
-    var excesoPeso = calcularExcesoDePeso(peso,pesoIdeal);
     console.log(notificaPesoIdeal(pesoIdeal));
-    console.log(notificaEstadoSobrepeso(excesoPeso));
+    console.log(notificaEstadoSobrepeso(calcularExcesoDePeso(peso,pesoIdeal)));
 }
 
 informePesoIdeal(1.52, 66, "mujer");
-
-
---
+++ Ejercicio2/app.js
@@ -0,0 +1,65 @@
+function submitName(){
+
+var fullName;
+
+//Obtener nombre
+var name = document.getElementById("input-name").value;
+fullName = name;
+
+//Obtener primer apellido
+var surname1 = document.getElementById("input-surname1").value;
+fullName = fullName + " " +surname1;
+
+//Obtenwer seundo apellido
+var surname2 = document.getElementById("input-surname2").value;
+fullName = fullName + " " +surname2;
+
+//pintamos el nombre completo
+document.getElementById("full-name").innerHTML = fullName;
+
+}
+
+//Registramos la funcion submitName como eventListener al clickar el bton enviar
+
+document.getElementById("button-submit").addEventListener("click",submitName);
+
+
+/** Romper el codigo
+ * 
+ * function getName(){
+ *  return document.getElementById("input-name").value;
+ * }
+ * function getSurname1(){
+ *  return document.getElementById("input-surname1").value;
+ * }
+ * function getSurname2(){
+ *  return document.getElementById("input-surname2").value;
+ * }
+ * 
+ * function getFullName(){
+ *    return getName() + " " + getSurname1() + " " + getSurname2();
+ * }
+ * 
+ * function submitName(){
+ *    return document.getElementById("full-name").innerTextL = getFullName();
+ * }
+ * 
+ * 
+ * ARROW FUNCTION
+ * var getName = () =>  document.getElementById("input-name").value;
+ * 
+ * var getSurname1 = () => document.getElementById("input-surname1").value;
+ * 
+ * var getSurname2 = () => document.getElementById("input-surname2").value;
+ * 
+ * var getFullName = () => getName() + " " + getSurname1() + " " + getSurname2();
+ * 
+ * var submitName = () => {
+ *     document.getElementById("full-name").innerTextL = getFullName();
+ * }
+ * 
+ * document.getElementById("input-name").addEventListener("keyup",submitName);
+ * document.getElementById("input-surname1").addEventListener("keyup",submitName);
+ * document.getElementById("input-surname2").addEventListener("keyup",submitName);
+ * 
+ */
