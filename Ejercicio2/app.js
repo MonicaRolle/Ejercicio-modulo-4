8bbf1e43e6753410ee9636a82adc14cf92130035
Monica <monica.rolle13@gmail.com>
2021-02-22 18:09:48 +0100

    Final

--- Ejercicio2/app.js
+++ Ejercicio2/app.js
@@ -1,27 +1,27 @@
-function submitName(){
+// function submitName(){
 
-var fullName;
+// var fullName;
 
-//Obtener nombre
-var name = document.getElementById("input-name").value;
-fullName = name;
+// //Obtener nombre
+// var name = document.getElementById("input-name").value;
+// fullName = name;
 
-//Obtener primer apellido
-var surname1 = document.getElementById("input-surname1").value;
-fullName = fullName + " " +surname1;
+// //Obtener primer apellido
+// var surname1 = document.getElementById("input-surname1").value;
+// fullName = fullName + " " +surname1;
 
-//Obtenwer seundo apellido
-var surname2 = document.getElementById("input-surname2").value;
-fullName = fullName + " " +surname2;
+// //Obtenwer seundo apellido
+// var surname2 = document.getElementById("input-surname2").value;
+// fullName = fullName + " " +surname2;
 
-//pintamos el nombre completo
-document.getElementById("full-name").innerHTML = fullName;
+// //pintamos el nombre completo
+// document.getElementById("full-name").innerHTML = fullName;
 
-}
+// }
 
-//Registramos la funcion submitName como eventListener al clickar el bton enviar
+// //Registramos la funcion submitName como eventListener al clickar el bton enviar
 
-document.getElementById("button-submit").addEventListener("click",submitName);
+// document.getElementById("button-submit").addEventListener("click",submitName);
 
 
 /** Romper el codigo
@@ -43,23 +43,19 @@ document.getElementById("button-submit").addEventListener("click",submitName);
  * function submitName(){
  *    return document.getElementById("full-name").innerTextL = getFullName();
  * }
- * 
- * 
- * ARROW FUNCTION
- * var getName = () =>  document.getElementById("input-name").value;
- * 
- * var getSurname1 = () => document.getElementById("input-surname1").value;
- * 
- * var getSurname2 = () => document.getElementById("input-surname2").value;
- * 
- * var getFullName = () => getName() + " " + getSurname1() + " " + getSurname2();
- * 
- * var submitName = () => {
- *     document.getElementById("full-name").innerTextL = getFullName();
- * }
- * 
- * document.getElementById("input-name").addEventListener("keyup",submitName);
- * document.getElementById("input-surname1").addEventListener("keyup",submitName);
- * document.getElementById("input-surname2").addEventListener("keyup",submitName);
- * 
  */
+// ARROW FUNCTION
+var getName = () => document.getElementById("input-name").value;
+var getSurname1 = () => document.getElementById("input-surname1").value;
+
+var getSurname2 = () => document.getElementById("input-surname2").value;
+
+var getFullName = () => getName() + " " + getSurname1() + " " + getSurname2();
+
+var submitName = () => {
+    document.getElementById("full-name").innerText = getFullName();
+}
+
+document.getElementById("input-name").addEventListener("keyup", submitName);
+document.getElementById("input-surname1").addEventListener("keyup", submitName);
+document.getElementById("input-surname2").addEventListener("keyup", submitName);
